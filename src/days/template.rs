477c412d8e55b65ba971c2758bd77caca2606740
Roger Coll <rogercoll@protonmail.com>
2022-12-02 18:53:35 +0100

    add tests to template file

--- src/days/template.rs
+++ src/days/template.rs
@@ -5,3 +5,14 @@ pub fn part1(input: String) -> String {
 pub fn part2(input: String) -> String {
     "TODO".to_string()
 }
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parts() {
+        assert_eq!("TODO", part1("".to_string()));
+        assert_eq!("TODO", part2("".to_string()));
+    }
+}
