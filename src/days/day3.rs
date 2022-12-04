f1e773dcd22bca01e549d6268c0df577d63c37a2
Roger Coll <rogercoll@protonmail.com>
2022-12-04 12:58:07 +0100

    add day3 part 2

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -21,8 +21,30 @@ pub fn part1(input: String) -> String {
         .to_string()
 }
 
+fn get_bitmap(ruckstack: &str) -> u64 {
+    ruckstack
+        .chars()
+        .enumerate()
+        .fold(0_u64, |mut bitmap, item| {
+            let mut pos: usize = if item.1.is_uppercase() { 26 } else { 0 };
+            pos += item.1.to_digit(36).unwrap() as usize - 10;
+            bitmap |= 1 << pos;
+            bitmap
+        })
+}
+
 pub fn part2(input: String) -> String {
-    "TODO".to_string()
+    input
+        .split('\n')
+        .collect::<Vec<_>>()
+        .chunks_exact(3)
+        .map(|chunk| {
+            let bitmap = get_bitmap(chunk[0]) & get_bitmap(chunk[1]) & get_bitmap(chunk[2]);
+            // returns the LO bit set to 1
+            bitmap.trailing_zeros() + 1
+        })
+        .sum::<u32>()
+        .to_string()
 }
 
 #[cfg(test)]
