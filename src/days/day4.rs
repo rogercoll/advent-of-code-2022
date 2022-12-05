ceee415dda989096eef8076773e51a8dcd7e02b6
Roger Coll <rogercoll@protonmail.com>
2022-12-05 11:34:04 +0100

    improve day4

--- src/days/day4.rs
+++ src/days/day4.rs
@@ -27,10 +27,7 @@ pub fn part2(input: String) -> String {
         .split('\n')
         .filter(|line| {
             let ranges = parse_line(line).unwrap();
-            (ranges.0 >= ranges.2 && ranges.0 <= ranges.3)
-                | (ranges.1 >= ranges.2 && ranges.1 <= ranges.3)
-                | (ranges.2 >= ranges.0 && ranges.2 <= ranges.1)
-                | (ranges.3 >= ranges.0 && ranges.3 <= ranges.1)
+            ranges.1 >= ranges.2 && ranges.0 <= ranges.3
         })
         .count()
         .to_string()
