252ba55b0d3db492b182584dedb4dce47abac3fd
Roger Coll <rogercoll@protonmail.com>
2022-12-07 16:12:39 +0100

    add day7

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ fn main() {
         (4, [days::day4::part1, days::day4::part2]),
         (5, [days::day5::part1, days::day5::part2]),
         (6, [days::day6::part1, days::day6::part2]),
+        (7, [days::day7::part1, days::day7::part2]),
     ]);
     process_args(fns);
 }
